720a52a613b36f68dc6c6e5faadf9183d4b8ef7a
Nvtai <buiblue123@gmail.com>
2025-03-15 23:23:58 +0700

    Add RelayCommand

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+namespace PRN212.Command;
+
+public class RelayCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+    
+    private Predicate<object> _CanExecute;
+
+    private Action<object> _Execute;
+
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+        _CanExecute = canExecute;
+        _Execute = execute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _CanExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _Execute(parameter);
+    }
+
+}
